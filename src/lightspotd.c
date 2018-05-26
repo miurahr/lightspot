8b61abd9fe11b4ea87bab0f0df7e0dcff3a495aa
Hiroshi Miura <miurahr@linux.com>
2018-05-26 15:55:45 +0900

    Revert HIDAPI based
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- src/lightspotd.c
+++ src/lightspotd.c
@@ -15,8 +15,9 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include "hidapi.h"
 
-#include "libusb.h"
+#define MAX_STR 255
 
 const unsigned char* reset_cmd =
         (const unsigned char *) "\x10\xff\x81\x00\x00\x00\x00";
@@ -73,219 +74,108 @@ void printhex(unsigned char *buf, int len) {
     fprintf(stderr, "\n");
 }
 
-typedef struct lightspot_device_ {
-    libusb_device *device;
-    libusb_device_handle *handle;
-} lightspot_device;
+hid_device *lightspot_device_connect() {
+    int res;
+    wchar_t wstr[MAX_STR];
+    hid_device *handle;
 
-static lightspot_device *new_device() {
-    lightspot_device *dev = (lightspot_device *)(calloc(1, sizeof(lightspot_device)));
-    return dev;
-}
+    struct hid_device_info *devs, *cur_dev, *target_dev;
 
-static void free_device(lightspot_device *dev) {
-    free(dev);
-}
-
-lightspot_device *lightspot_find_device(libusb_context *ctx) {
-    libusb_device **list;
-    struct libusb_device_descriptor desc;
-    ssize_t cnt;
-    int res=0;
-    lightspot_device *dev;
-
-    cnt = libusb_get_device_list(ctx, &list);
-    if (cnt < 0) {
+    devs = hid_enumerate(0x046d, 0xc53e);
+    cur_dev = devs;
+    target_dev = NULL;
+    while (cur_dev) {
+        if (cur_dev->interface_number == 2) {
+            target_dev = cur_dev;
+        }
+        cur_dev = cur_dev->next;
+    }
+    if (target_dev == NULL) {
         return NULL;
     }
 
-    for (int i = 0; i < cnt; i++) {
-        int r = libusb_get_device_descriptor(list[i], &desc);
-        if (r < 0) {
-            break;
-        }
-        if (desc.idVendor == 0x046d &&
-            desc.idProduct == 0xc53e) {
-
-            // Check device
-            struct libusb_config_descriptor *config;
-            libusb_get_config_descriptor(list[i], 0, &config);
-            if ((int) config->bNumInterfaces == 3) {
-                const struct libusb_interface *inter;
-                const struct libusb_interface_descriptor *interdesc;
-                const struct libusb_endpoint_descriptor *epdesc;
-
-                inter = &config->interface[2];
-                interdesc = &inter->altsetting[0];
-                epdesc = &interdesc->endpoint[0];
-                if ((int) interdesc->bInterfaceNumber == 2 &&
-                    (int) interdesc->bInterfaceClass == LIBUSB_CLASS_HID &&
-                    (int) interdesc->bInterfaceSubClass == 0 &&
-                    (int) epdesc->bDescriptorType == 5 &&
-                    (int) epdesc->bEndpointAddress == 0x83) {
-                    fprintf(stderr, "Found device.\n");
-                    dev = new_device();
-                    dev->device = list[i];
-                    res=1;
-                }
-            }
-            libusb_free_config_descriptor(config);
-        }
-    }
-    libusb_free_device_list(list, 1);
-
-    if (res) {
-        return dev;
-    } else {
+    handle = hid_open_path(target_dev->path);
+    if (handle == NULL) {
+        fprintf(stderr, "Fail to open a Spotlight USB Receiver device.\n");
         return NULL;
     }
+    res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
+    fprintf(stderr, "Connected to %ls", wstr);
+    res = hid_get_product_string(handle, wstr, MAX_STR);
+    fprintf(stderr, " %ls\n", wstr);
+    hid_free_enumeration(devs);
+
+    return handle;
 }
 
-int lightspot_device_connect(lightspot_device *dev) {
-    int res;
-    libusb_device *device = dev->device;
-    libusb_device_handle *handle;
 
-    res = libusb_open(device, &handle);
-    if (res == 0) {
-        res = libusb_kernel_driver_active(handle, 2);
-        if (res == 1) {
-            res = libusb_detach_kernel_driver(handle, 2);
-			if( res != 0 )
-			{
-				return res;
-			}
-        }
-        fprintf(stderr, "Successfully open the device.\n");
-        dev->handle = handle;
-    }
-    return res;
-}
-
-int lightspot_write(lightspot_device *dev, const unsigned char *data, unsigned int length) {
-    int res;
-    unsigned int report_number = data[0];
-    int skipped_report_id = 0;
-
-	if (report_number == 0x0) {
-		data++;
-		length--;
-		skipped_report_id = 1;
-	}
-
-    res = libusb_control_transfer(dev->handle,
-                                  LIBUSB_REQUEST_TYPE_CLASS|LIBUSB_RECIPIENT_INTERFACE|LIBUSB_ENDPOINT_OUT,
-                                  0x09/*HID Set_Report*/,
-                                  (2/*HID output*/ << 8) | report_number,
-                                  2,
-                                  (unsigned char*)data,
-                                  (uint16_t) length,
-                                  1000/*timeout millis*/);
-    if (res < 0)
-        return -1;
-
-    if (skipped_report_id)
-	    length++;
-
-    return length;
-}
-
-int lightspot_read(lightspot_device *dev, unsigned char *buf, int length)
-{
-	int actual;
-
-	libusb_interrupt_transfer(dev->handle, 0x83, buf, length, &actual, 5000);
-	return actual;
-}
-
-void lightspot_init(lightspot_device *dev) {
+void lightspot_init(hid_device *dev) {
     int res;
     unsigned char buf[65];
 
     fprintf(stderr, "send reset_cmd:");
-    lightspot_write(dev, reset_cmd, 7);
-    res = lightspot_read(dev, buf, 65);
+    hid_write(dev, reset_cmd, 7);
+    res = hid_read(dev, buf, 65);
     printhex(buf, res);
 
     fprintf(stderr, "send start_cmd:");
-    lightspot_write(dev, start_cmd, 7);
-    res = lightspot_read(dev, buf, 65);
+    hid_write(dev, start_cmd, 7);
+    res = hid_read(dev, buf, 65);
     printhex(buf, res);
 
     for (int i=0; i<2; i++) {
         fprintf(stderr, "send init_cmd:");
-        lightspot_write(dev, init_cmds[i], 7);
-        res = lightspot_read(dev, buf, 65);
+        hid_write(dev, init_cmds[i], 7);
+        res = hid_read(dev, buf, 65);
         printhex(buf, res);
     }
-    res = lightspot_read(dev, buf, 65);
+    res = hid_read(dev, buf, 65);
     printhex(buf, res);
 
     for(int i=0; i<4; i++) {
         fprintf(stderr, "send config_cmd:");
-        lightspot_write(dev, config_cmds[i], 7);
-        res = lightspot_read(dev, buf, 65);
+        hid_write(dev, config_cmds[i], 7);
+        res = hid_read(dev, buf, 65);
         printhex(buf, res);
     }
     for(int i=0; i<6; i++) {
         fprintf(stderr, "send config2_cmd:");
-        lightspot_write(dev, config2_cmds[i], 7);
-        res = lightspot_read(dev, buf, 65);
+        hid_write(dev, config2_cmds[i], 7);
+        res = hid_read(dev, buf, 65);
         printhex(buf, res);
-    }}
+    }
+}
 
 void handle_linux_ev(unsigned char *buf, int len) {
     // TODO: implement me.
     printhex(buf, len);
 }
 
-void lightspot_device_disconnect(lightspot_device *dev) {
-    libusb_close(dev->handle);
-    free_device(dev);
-}
-
 int main(int argc, char *argv[]) {
     int res,i;
-    libusb_context *ctx;
-    lightspot_device *dev;
+    hid_device *dev;
     unsigned char buf[65];
 
-    res = libusb_init(&ctx);
+    res = hid_init();
     if (res < 0) {
         exit(1);
     }
-
-    for(i=0; i<65536; i++) { // fixme: exit with signal
-        dev = lightspot_find_device(ctx);
+    for (;;) {
+        dev = lightspot_device_connect();
         if (dev) {
-            res =lightspot_device_connect(dev);
-            if (res== 0) {
-                lightspot_init(dev);
-                /*
-                for (uint16_t s = 0; s < 256; s++) {
-                    unsigned char cmd[7];
-                    memcpy((void *)cmd_poll, cmd, sizeof(cmd));
-                    cmd[4] = (unsigned char) (s & 0xFF);
-                    lightspot_write(dev, cmd, 7);
-                    res = lightspot_read(dev, buf, 65);
-                    if (res < 0) {
-                        // error, probably removed an usb device.
-                        break;
-                    } else if (res > 0) {
-                        handle_linux_ev(buf, res);
-                    }
-                    usleep(20000);
-                }
-                */
-                lightspot_device_disconnect(dev);
-                break;
-            } else {
-                fprintf(stderr, "Device open error: %d.\n", res);
-                break;
+            lightspot_init(dev);
+            for(int s=0; s<256; s++) {
+                unsigned char cmd[7];
+                memcpy((void *) cmd_poll, cmd, sizeof(cmd));
+                cmd[4] = (unsigned char) (s & 0xFF);
+                hid_write(dev, cmd, 7);
+                res = hid_read(dev, buf, 65);
+                handle_linux_ev(buf, res);
             }
+            hid_close(dev);
         } else {
-            usleep(3000 * 1000);
+            usleep(3000*1000);
         }
     }
-    libusb_exit(ctx);
 }
+
